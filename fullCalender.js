fc807e59356a7ce9cb901a51e952d3553fefde0b
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2019-10-07 16:45:14 +0600

    Create fullCalender.js

---
+++ fullCalender.js
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8' />
+<link href='../packages/core/main.css' rel='stylesheet' />
+<link href='../packages/daygrid/main.css' rel='stylesheet' />
+<link href='../packages/timegrid/main.css' rel='stylesheet' />
+<script src='../packages/core/main.js'></script>
+<script src='../packages/interaction/main.js'></script>
+<script src='../packages/daygrid/main.js'></script>
+<script src='../packages/timegrid/main.js'></script>
+<script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      timeZone: 'local', // যদি GMT থাকে তাহলে তাকে লোকাল ফর্মেটে দেখাবে।
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      defaultDate: '2019-10-07',
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        var title = prompt('Event Title:');
+        if (title) {
+          calendar.addEvent({
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+          })
+        }
+        calendar.unselect()
+      },
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+       
+        {
+          title: 'Test',
+          start: 1570444495713,  // এটা GMT ফর্মেটে টাইম যা GMT তে সকাল ১০.৩৪ GMT.
+        },
+        
+      ]
+    });
+
+    calendar.render();
+  });
+
+</script>
+<style>
+
+  body {
+    margin: 40px 10px;
+    padding: 0;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  #calendar {
+    max-width: 900px;
+    margin: 0 auto;
+  }
+
+</style>
+</head>
+<body>
+
+  <div id='calendar'></div>
+
+</body>
+</html>
+
+
+
+//////////////////////////////////////////////////////////////
+new org.joda.time.DateTime( org.joda.time.DateTimeZone.UTC  ).getMillis এটা GMT ফর্মেটে সময় তৈরী করে।
+আমরা ডিবিতে GMT milliseconds রাখব। কিন্তু কেলেন্ডার টাইমজোন অনুসারে সময় দেখাবে। 
