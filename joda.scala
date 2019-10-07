9502c326d97f7d98311a7abc0865e92465b6845e
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2019-10-07 17:26:52 +0600

    Update joda.scala

--- joda.scala
+++ joda.scala
@@ -3,8 +3,11 @@ val jodaDateTime: DateTime = new org.joda.time.DateTime(org.joda.time.DateTimeZo
 val format = DateTimeFormat.forPattern("yyyy-MM-dd")
 println(format.print(jodaDateTime)) // 2019-10-07
 
-println(Instant.now()) // 2019-10-07T11:17:58.023Z 
-println(jodaDateTime.withZone(DateTimeZone.forID("Asia/Dhaka"))) // 2019-10-07T17:17:58.023+06:00
+println(Instant.now()) // 2019-10-07T11:25:12.398Z
+println(jodaDateTime.withZone(DateTimeZone.forID("Asia/Dhaka"))) // 2019-10-07T17:25:12.398+06:00
+println(jodaDateTime.withZone(DateTimeZone.forID("Asia/Kolkata"))) // 2019-10-07T16:55:12.398+05:30
+
+
 
 
 
